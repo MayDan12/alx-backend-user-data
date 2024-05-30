34aade2f96266815a0d2e7ac81eaa5fd2a2bb1c2
MayDan12 <oluwamayowadan@gmail.com>
2024-05-30 13:17:36 +0000

    Testing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,6 @@ A function that returns the log massage obfuscated
 import re
 
 
-
 def filter_datum(fields, redaction, message, separator=" "):
-    pattern = r"(?:"+ separator + r"(?:(?:"+ "|".join(fields)+ r")\b.+?))"
+    pattern = r"(?:" + separator + r"(?:(?:" + "|".join(fields) + r") \b.+?))"
     return re.sub(pattern, redaction, message)
